67eb28ece7c75c20954d8e031121ce6ecba16077
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-14 22:53:33 +0530

    set Spannable String

--- app/src/main/java/com/example/eatoes_assignment/ui/view/LoginActivity.kt
+++ app/src/main/java/com/example/eatoes_assignment/ui/view/LoginActivity.kt
@@ -16,9 +16,32 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        //setSpannableString()
+        setSpannableString()
     }
 
+    //Setting the SpannableString
+    private fun setSpannableString() {
+        val signUpText = SpannableString(getString(R.string.sign_up))
+        val noAccountText = SpannableString(getString(R.string.no_eatoes_account))
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                //send back
+                Toast.makeText(this@LoginActivity, "Sign Up First", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+                ds.color = ds.linkColor
+            }
+        }
+        signUpText.setSpan(clickableSpan, 0, signUpText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        signUpTextView.movementMethod = LinkMovementMethod.getInstance()
+        signUpTextView.text = signUpText
+        noAccountText.setSpan(clickableSpan, 0, signUpText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        noAccountTextView.movementMethod = LinkMovementMethod.getInstance()
+        noAccountTextView.text = noAccountText
+    }
 
 
 }
