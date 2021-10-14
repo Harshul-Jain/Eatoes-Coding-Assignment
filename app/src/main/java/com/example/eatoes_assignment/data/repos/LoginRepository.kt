f93f8859809f54bc8dc3ee44e67e5eccb1a7c718
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-15 01:18:25 +0530

    Added LoginRepository Object

---
+++ app/src/main/java/com/example/eatoes_assignment/data/repos/LoginRepository.kt
@@ -0,0 +1,10 @@
+package com.example.eatoes_assignment.data.repos
+
+import com.example.eatoes_assignment.data.api.Client
+import com.example.eatoes_assignment.data.modals.LoginRequest
+
+object LoginRepository {
+
+    suspend fun login(email: String, password: String) =
+        Client.api.login(LoginRequest(email, password))
+}
