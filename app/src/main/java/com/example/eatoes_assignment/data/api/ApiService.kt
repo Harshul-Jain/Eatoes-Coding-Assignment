e412bc2eed56896cd927f38ba39a0c81e42bd44b
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-15 00:34:45 +0530

    Added ApiService Interface

---
+++ app/src/main/java/com/example/eatoes_assignment/data/api/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.eatoes_assignment.data.api
+
+import com.example.eatoes_assignment.data.modals.LoginRequest
+import com.example.eatoes_assignment.data.modals.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @POST(Constants.LOGIN_URL)
+    @FormUrlEncoded
+    suspend fun login(@Body request:LoginRequest):Response<LoginResponse>
+}
